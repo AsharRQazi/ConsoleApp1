cd805614b0ce83c43a9cbacfa3dac24fbca1bf23
KomalKukadiya <komal_kukadiya@tegasys.co.uk>
2023-07-25 10:39:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StudentAverageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Student Marks Calculator");
+
+            Console.Write("Enter your name: ");
+            string studentName = Console.ReadLine();
+
+            double[] subjectScores = new double[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Enter score for subject {i + 1}: ");
+                while (!double.TryParse(Console.ReadLine(), out subjectScores[i]))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid score.");
+                    Console.Write($"Enter score for subject {i + 1}: ");
+                }
+            }
+
+            double averageScore = (subjectScores[0] + subjectScores[1] + subjectScores[2]) / 3;
+
+            Console.WriteLine($"Hello, {studentName}!");
+            Console.WriteLine($"Your average score is: {averageScore:F2}");
+
+            Console.WriteLine("Thank you for using the Student Average Calculator!");
+        }
+    }
+}
